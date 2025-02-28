85fd5c57c988c18a23b5dbd77b5714b7631d8655
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-02-28 17:35:32 +0000

    Added circ_circle_tau.py and constants.py

---
+++ circ_circle_tau.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Feb. 28, 2025
+"""
+This program asks the user for the radius of a circle in cm.
+It then calculates and displays the circumference using tau.
+"""
+import constants
+
+
+def main():
+    # Get the radius from the user.
+    radius = float(input("Enter the radius of the circle in (cm): "))
+
+    # Calculate the circumference.
+    circumference = constants.TAU * radius
+
+    # Display the circumference.
+    print(f"\nCircumference = {circumference} cm")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Feb. 28, 2025
+# This module contains constants.
+
+# Best to use 2 * math.pi, but for this daily assignment, it is fine.
+TAU = 6.28
